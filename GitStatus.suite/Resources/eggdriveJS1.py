d520a652d38ea97ec33c39be2dfc915f31463e3e
reenakuni <reena.kuni@gmail.com>
2018-10-04 11:30:57 -0600

    first commit

---
+++ GitStatus.suite/Resources/eggdriveJS1.py
@@ -0,0 +1,23 @@
+import xmlrpclib
+
+
+# connect to eggPlant instance
+server = xmlrpclib.ServerProxy("http://127.0.0.1:5400")
+
+
+# Start the session
+server.startsession("/Users/jeanette/Documents/eggDriveTests.suite")
+
+# Verify that you can pass suiteinfo() without an argument in drive mode
+
+# Failed before 
+try:
+   server.execute("put suiteinfo()")
+except:
+   server.endsession("")
+
+# Worked before
+# server.execute("put suiteinfo(\"/Users/jeanette/Documents/TestProjects/ePF/CornerStoneWorkingCopy/Jeannette/testsuites_no_scm/eggDriveTests.suite\")")
+
+
+server.endsession("")
